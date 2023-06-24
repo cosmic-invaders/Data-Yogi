a58d6292231bb9763df993d46fe1bd7581507f23
jaswanthreddy-007 <88081034+jaswanthreddy-007@users.noreply.github.com>
2023-06-24 22:41:23 +0530

    cleanup

--- test2.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-import streamlit.components.v1 as components
-pf=open("F:/code/mistrel/nl4dv/visualization.html",'r')
-components.html(pf.read(),width=400,height=400,scrolling=True)
